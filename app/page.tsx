f92bde9f1418f55ecfcf2dc6b6fa58921466eb51
Dimebagou <bersonvictor@gmail.com>
2024-06-06 11:46:01 +0200

    background color + login redirection button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import LoginButton from "@/components/auth/LoginButton";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-    return <Button size={"lg"}>Click !</Button>;
+    return (
+        <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-500 to-purple-800 ">
+            <div className="space-y-6 text-center">
+                <h1 className="text-6xl  drop-shadow-md">🔐 Connexion</h1>
+                <p className="text-lg">Connectez-vous à Stream Line</p>
+                <div>
+                    <LoginButton mode="redirect">
+                        <Button variant="secondary" size="lg">
+                            Se connecter
+                        </Button>
+                    </LoginButton>
+                </div>
+            </div>
+        </main>
+    );
 }

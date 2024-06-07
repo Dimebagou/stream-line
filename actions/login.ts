5e8607b3587dd413e7cfaa9b327a8915573c35da
Dimebagou <bersonvictor@gmail.com>
2024-06-07 12:59:12 +0200

    database and prisma setup

--- actions/login.ts
+++ actions/login.ts
@@ -5,8 +5,6 @@ import * as z from "zod";
 import { LoginSchema } from "@/schemas";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
-    console.log(values);
-
     const validatedFields = LoginSchema.safeParse(values);
 
     if (!validatedFields.success) {
--- actions/register.ts
+++ actions/register.ts
@@ -5,8 +5,6 @@ import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-    console.log(values);
-
     const validatedFields = RegisterSchema.safeParse(values);
 
     if (!validatedFields.success) {
